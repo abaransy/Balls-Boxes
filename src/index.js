266b481d162a6bef4f8ca411ed51f9e7cd547b6b
Aatef Baransy <baransymusic@gmail.com>
2020-09-09 16:52:24 -0700

    balls can now be placed in boxes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     let draggableBalls = new Draggable(ballsContainer, { 
         draggable: 'img', 
     });
+
+    let currentBall; 
+    let [firstboxBall, secondBoxBall, thirdBoxBall] = Array.from(document.getElementsByClassName("inbox"))[0].children
+    let [firstBox, secondBox, thirdBox] = Array.from(document.getElementsByClassName("box")); 
+    let x; 
+    let y; 
     
     const triggerMouseEvent = (node, eventType) => {
         const clickEvent = document.createEvent('MouseEvents');
@@ -13,13 +19,56 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const handleDragging = (e) => {
-        if (e.sensorEvent.data.clientX < 220  || 
-            e.sensorEvent.data.clientX > 850 || 
-            e.sensorEvent.data.clientY < 107 ||
-            e.sensorEvent.data.clientY > 500
+        currentBall = Array.from(document.getElementsByClassName("draggable--original"))[0];
+        x = e.sensorEvent.data.clientX; 
+        y = e.sensorEvent.data.clientY;
+
+        if (x < 220  || 
+            x > 850 || 
+            y < 107 ||
+            y > 500
         ) triggerMouseEvent(document.body, "mouseup"); 
+
+        if (y > 410) {
+            if (x > 340 && x < 375) {
+                firstBox.style.borderBottom = "1px solid red"; 
+                secondBox.style.border = "none"; 
+                thirdBox.style.border = "none"; 
+            } else if (x > 530 && x < 565) {
+                firstBox.style.border = "none";
+                secondBox.style.borderBottom = "1px solid red";
+                thirdBox.style.border = "none"; 
+            } else if (x > 700 && x < 735) {
+                firstBox.style.border = "none";
+                secondBox.style.border = "none";
+                thirdBox.style.borderBottom = "1px solid red"; 
+            }
+        } else {
+            firstBox.style.border = "none";
+            secondBox.style.border = "none";
+            thirdBox.style.border = "none";
+        }
     }
 
+    const handleStop = (e) => {
+        if (y > 410) {
+            if (x > 340 && x < 375) {
+                firstboxBall.style.visibility = "visible"; 
+                currentBall.style.visibility = "hidden"; 
+            } else if (x > 530 && x < 565) {
+                secondBoxBall.style.visibility = "visible";
+                currentBall.style.visibility = "hidden"; 
+            } else if (x > 700 && x < 735) {
+                thirdBoxBall.style.visibility = "visible";
+                currentBall.style.visibility = "hidden"; 
+            }
+        } 
+
+        firstBox.style.border = "none";
+        secondBox.style.border = "none";
+        thirdBox.style.border = "none";
+    }
 
     draggableBalls.on('drag:move', (e) => handleDragging(e))
+    draggableBalls.on('drag:stop', (e) => handleStop(e)); 
 })
