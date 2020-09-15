58452ee32ca058c3cea439b078f6c68e41761029
Aatef Baransy <baransymusic@gmail.com>
2020-09-14 17:44:05 -0700

    added red slash to indicate music not playing

--- src/audio_manager.js
+++ src/audio_manager.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const player = document.getElementById("player");
     const togglePlayerButton = document.getElementById('toggle_player'); 
+    const muteSlash = document.getElementById("mute"); 
     let isPlaying = false;
     
     const togglePlay = () => {
         isPlaying ? player.pause() : player.play();
+        isPlaying ? muteSlash.style.visibility = "visible" : muteSlash.style.visibility = "hidden";
     };
     
     player.onplaying = () => {
